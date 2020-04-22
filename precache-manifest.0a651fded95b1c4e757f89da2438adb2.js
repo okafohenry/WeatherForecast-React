09c6bb2ddd09eeecb93634fb2e43f8fbba800e01
okafohenrie <okafohenrie@gmail.com>
2020-04-22 12:54:39 +0100

    Updates

---
+++ precache-manifest.0a651fded95b1c4e757f89da2438adb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f12d8a99ea79d73647f9ecfef569d3ed",
+    "url": "/WeatherForecast-React/static/media/loading_img.f12d8a99.gif"
+  },
+  {
+    "revision": "74b76dc11d9cf339836a",
+    "url": "/WeatherForecast-React/static/js/runtime~main.74b76dc1.js"
+  },
+  {
+    "revision": "54278acc788354b13242",
+    "url": "/WeatherForecast-React/static/js/main.54278acc.chunk.js"
+  },
+  {
+    "revision": "6ec8f61c0f6050a47903",
+    "url": "/WeatherForecast-React/static/js/1.6ec8f61c.chunk.js"
+  },
+  {
+    "revision": "54278acc788354b13242",
+    "url": "/WeatherForecast-React/static/css/main.991df7b0.chunk.css"
+  },
+  {
+    "revision": "ed8f5733da176716f2a07d1e28190cd3",
+    "url": "/WeatherForecast-React/index.html"
+  }
+];
